7f192a90ca856e46450554a17c13798259840f0c
lucaasmz <103609817+lucaasmz@users.noreply.github.com>
2022-09-08 12:56:26 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Hola Mundo</h1>
+
+    <?php
+    $Saludo = "Hola Mundo";
+    echo $Saludo;
+
+
+    $variable1 = 15;
+    $variable2 = 90;
+
+    echo $variable1;
+    echo $variable2;
+
+    echo $variable1 + $variable2 ;
+    echo $variable1 - $variable2 ;
+    echo $variable1 * $variable2 ;
+    echo $variable1 / $variable2 ;
+
+
+
+    $suma == $variable1 + $variable2;
+    $resta == $variable1 + $variable2;
+
+
+
+
+     ?>
+
+  </body>
+</html>
